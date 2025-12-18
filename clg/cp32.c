3a4b39b469c0b561aedd1b3fa22ec13cd11f0f04
amitmahto-code <mahtoamit6678@gmail.com>
2025-12-18 23:21:47 +0530

    all program of collage

--- clg/cp32.c
+++ clg/cp32.c
@@ -1,12 +1,16 @@
 //write a program that input 10 number display total of all of number 
 #include<stdio.h>
+#include<math.h>
 int main(){
-    int n,sum=0;
+    int n;
+    int sum= 0;
     for(int i=1;i<=10;i++){
         printf("enter number");
         scanf("%d",&n);
         sum+=n;
     }
-    printf("sum%d",sum);
+    printf("sum%d\n",sum);
+    printf("avg%d",sum/10);
+
     return 0;
 }
