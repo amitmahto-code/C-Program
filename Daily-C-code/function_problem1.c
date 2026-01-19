375f050a873b0a506d2b0fa9d1872edde0d378f8
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-19 20:31:03 +0530

    basic function problem

---
+++ Daily-C-code/function_problem1.c
@@ -0,0 +1,11 @@
+//write a function named greet that prints "hello","world!" when called
+#include<stdio.h>
+void  greet();
+
+int main(){
+    greet();
+    return 0;
+}
+void greet(){
+    printf("hello World");
+}
